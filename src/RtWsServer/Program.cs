ef09b22486c945fc93cd6476f46cce7377b4c8d8
Victor Haydin <victor.haydin@eleks.com>
2013-11-05 18:16:43 +0200

    Implemented basic dashboard with console and chart.

---
+++ src/RtWsServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Alchemy;
+using Alchemy.Classes;
+
+namespace RtWsServer
+{
+    class Program
+    {
+        private static Random _rand = new Random();
+        
+        
+        static void Main(string[] args)
+        {
+            var state = 10;
+            using (var service = new WebSocketService())
+            {
+                while (true)
+                {
+                    PublishState(state, service);
+                    PrintState(state);
+                    state = ModifyState(state);
+                    Thread.Sleep(500 + _rand.Next(500));
+                }
+            }
+        }
+
+        private static int ModifyState(int state)
+        {
+            var newState = state + _rand.Next(5) - 2;
+            return newState >= 0 ? newState : 0;
+        }
+
+        private static void PrintState(int state)
+        {
+            Console.WriteLine(state);
+        }
+
+        private static void PublishState(int state, WebSocketService service)
+        {
+            service.Publish(state);
+        }
+    }
+}
---
+++ src/RtWsServer/WebSocketService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Alchemy;
+using Alchemy.Classes;
+
+namespace RtWsServer
+{
+    class WebSocketService : IDisposable
+    {
+        private WebSocketServer _wsServer = new WebSocketServer(8181);
+        private List<UserContext> _users = new List<UserContext>();
+        private ConcurrentQueue<int> _messages = new ConcurrentQueue<int>();
+        private Thread _workerThread;
+
+        public WebSocketService()
+        {
+            _workerThread = new Thread(ProcessMessages);
+            _wsServer.OnConnected += context =>
+            {
+                Console.WriteLine("Connected: {0}", context.ClientAddress);
+                _users.Add(context); // is this thread safe?
+            };
+            _wsServer.OnDisconnect += context => Console.WriteLine("Disconnected: {0}", context.ClientAddress);
+            _wsServer.OnReceive += e => Console.WriteLine("Received data from client: {0}", e.DataFrame);
+            _wsServer.Start();
+            _workerThread.Start();
+        }
+
+        public void Dispose()
+        {
+            _workerThread.Abort();
+            _wsServer.Dispose();
+        }
+
+        public void Publish(int state)
+        {
+            _messages.Enqueue(state);
+        }
+
+        private void ProcessMessages()
+        {
+            int message;
+            while (true)
+            {
+                if (_messages.TryDequeue(out message))
+                {
+                    foreach (var userContext in _users)
+                    {
+                        userContext.Send(message.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
